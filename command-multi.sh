018c68a0755ba83d727ada485dc91f5cfa64c74d
Gagan Ichake <gaganichake@Gagans-MacBook-Air.local>
2020-09-19 09:27:12 -0500

    Force remove the Container if already running

--- command-multi.sh
+++ command-multi.sh
@@ -2,10 +2,19 @@
 
 echo "Credit: https://codefresh.io/docker-tutorial/create-docker-images-for-java/"
 
+# This only when testing on local machine
 # git clone https://github.com/gaganichake/docker-with-java-demos.git
 
 cd docker-multi-stage-build-demo
 
+# Build a docker image with <name>:<tag>
 docker build -t gaganichake/docker-multi-stage-build-demo:1.0 .
 
+# Stop the Container if it already running
+docker stop JavaAppContainer
+
+# Force remove the Container if still running
+docker rm -f JavaAppContainer
+
+# Run the Container
 docker run -d -p 8080:8080 --name JavaAppContainer gaganichake/docker-multi-stage-build-demo:1.0
